1bfbe3d7ee2ce0e892d523d682e6d31ded269bf6
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-11-10 23:01:23 -0800

    Restore original test script

--- src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -16,84 +16,29 @@ public class AuctionSearchTest {
 		String message = "Test message";
 		String reply = as.echo(message);
 		System.out.println("Reply: " + reply);
-		//System.out.println("");
 		
-		String query1 = "superman";
-		SearchResult[] basicResults1 = as.basicSearch(query1, 0, 0);
-		System.out.println("Basic Search Query: " + query1);
-		System.out.println("Received " + basicResults1.length + " results");
-		for(SearchResult result : basicResults1) {
-			//System.out.println(result.getItemId() + ": " + result.getName());
+		String query = "superman";
+		SearchResult[] basicResults = as.basicSearch(query, 0, 20);
+		System.out.println("Basic Seacrh Query: " + query);
+		System.out.println("Received " + basicResults.length + " results");
+		for(SearchResult result : basicResults) {
+			System.out.println(result.getItemId() + ": " + result.getName());
 		}
-		//System.out.println("");
 		
-		/*String query2 = "kitchenware";
-		SearchResult[] basicResults2 = as.basicSearch(query2, 0, 0);
-		System.out.println("Basic Search Query: " + query2);
-		System.out.println("Received " + basicResults2.length + " results");
-		for(SearchResult result : basicResults2) {
-			//System.out.println(result.getItemId() + ": " + result.getName());
-		}
-		//System.out.println("");*/
-		
-		/*String query3 = "star trek";
-		SearchResult[] basicResults3 = as.basicSearch(query3, 0, 0);
-		System.out.println("Basic Search Query: " + query3);
-		System.out.println("Received " + basicResults3.length + " results");
-		for(SearchResult result : basicResults3) {
-			//System.out.println(result.getItemId() + ": " + result.getName());
-		}
-		//System.out.println("");*/
-		
-		SearchConstraint[] constraints1 = {new SearchConstraint(FieldName.BuyPrice, "5.99")};
-		SearchResult[] advancedResults1 = as.advancedSearch(constraints1, 0, 20);
+		SearchConstraint constraint =
+		    new SearchConstraint(FieldName.BuyPrice, "5.99"); 
+		SearchConstraint[] constraints = {constraint};
+		SearchResult[] advancedResults = as.advancedSearch(constraints, 0, 20);
 		System.out.println("Advanced Seacrh");
-		System.out.println("Received " + advancedResults1.length + " results");
-		for(SearchResult result : advancedResults1) {
-			//System.out.println(result.getItemId() + ": " + result.getName());
+		System.out.println("Received " + advancedResults.length + " results");
+		for(SearchResult result : advancedResults) {
+			System.out.println(result.getItemId() + ": " + result.getName());
 		}
-		//System.out.println("");
-		
-		/*SearchConstraint[] constraints2 = {
-				new SearchConstraint(FieldName.ItemName, "pan"),
-				new SearchConstraint(FieldName.Category, "kitchenware")
-				};
-		SearchResult[] advancedResults2 = as.advancedSearch(constraints2, 0, 20);
-		System.out.println("Advanced Seacrh");
-		System.out.println("Received " + advancedResults2.length + " results");
-		for(SearchResult result : advancedResults2) {
-			//System.out.println(result.getItemId() + ": " + result.getName());
-		}
-		//System.out.println("");*/
-		
-		/*SearchConstraint[] constraints3 = {
-				new SearchConstraint(FieldName.ItemName, "Precious Moments"),
-				new SearchConstraint(FieldName.SellerId, "waltera317a")
-				};
-		SearchResult[] advancedResults3 = as.advancedSearch(constraints3, 0, 20);
-		System.out.println("Advanced Seacrh");
-		System.out.println("Received " + advancedResults3.length + " results");
-		for(SearchResult result : advancedResults3) {
-			//System.out.println(result.getItemId() + ": " + result.getName());
-		}
-		//System.out.println("");*/
-		
-		/*SearchConstraint[] constraints4 = {
-				new SearchConstraint(FieldName.EndTime, "Dec-14-01 21:00:05")
-				};
-		SearchResult[] advancedResults4 = as.advancedSearch(constraints4, 0, 20);
-		System.out.println("Advanced Seacrh");
-		System.out.println("Received " + advancedResults4.length + " results");
-		for(SearchResult result : advancedResults4) {
-			//System.out.println(result.getItemId() + ": " + result.getName());
-		}
-		//System.out.println("");*/
 		
 		String itemId = "1497595357";
 		String item = as.getXMLDataForItemId(itemId);
 		System.out.println("XML data for ItemId: " + itemId);
 		System.out.println(item);
-		//System.out.println("");
 
 		// Add your own test here
 	}
